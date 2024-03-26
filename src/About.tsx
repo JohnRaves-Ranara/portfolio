9ad6534bb62c3f52575eb40cac26bb9c1bd95ce6
JohnRaves-Ranara <jrRanara@mcm.edu.ph>
2024-03-26 08:17:36 +0800

    changed about description

--- src/About.tsx
+++ src/About.tsx
@@ -10,7 +10,7 @@ export default function About()  {
                     <h1 className="text-[1.1rem] text-blue-400 font-hnd-black leading-[2rem] text-center lg:text-start">ABOUT ME</h1>
                     <h1 className='text-[1.3rem] font-hnd-black leading-[2rem] text-center lg:text-start'>Computer Science Student, Aspiring Software Engineer based in Davao, Philippines 📍</h1>
                     <p className='text-center lg:text-start font-hnd-thin text-[18px]'>Hey, I'm John Raves, and I'm a CS student with strong passion for building real world applications.</p>
-                    <p className='text-center lg:text-start font-hnd-thin text-[18px]'>I am actively enhancing my skills in programming. The tech-stack I am confident in are Flutter and HTML/CSS/JS</p>
+                    <p className='text-center lg:text-start font-hnd-thin text-[18px]'>I am actively enhancing my skills in programming. The tech-stack I am confident in are Flutter and HTML/CSS. I am currently working on my JS skills.</p>
                 </div>
             </div>
         </section>
